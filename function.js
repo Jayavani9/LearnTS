8e303dc2b8a9d5e9db730cb0f6c13128b514ff1b
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-09-12 10:02:53 -0700

    Update function.js

--- function.js
+++ function.js
@@ -13,3 +13,12 @@ console.log(ans);
 
 var msg = function (a, b) { return console.log(a + b); };
 msg(2, 3);
+
+//Arrow function
+var msg = function (a, b) { return console.log(a + b); };
+msg(2, 3);
+var dp = function (x, y, a) {
+    return x + y - a;
+};
+var ans1 = dp(3, 2, 4);
+console.log(ans1);
